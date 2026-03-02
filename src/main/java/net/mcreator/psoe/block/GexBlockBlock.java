92207f39bfe9f770a2d1661a3c121e55cc85b26f
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 <zeroi@LAPTOP-1DTC6RG9>
2026-03-02 22:20:04 +0700

    гкс апдт

---
+++ src/main/java/net/mcreator/psoe/block/GexBlockBlock.java
@@ -0,0 +1,28 @@
+package net.mcreator.psoe.block;
+
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.Mth;
+import net.minecraft.core.BlockPos;
+
+public class GexBlockBlock extends Block {
+	public GexBlockBlock() {
+		super(BlockBehaviour.Properties.of().sound(SoundType.GLASS).strength(0.8f, 3f).requiresCorrectToolForDrops().friction(0.8f).speedFactor(1.1f).jumpFactor(0.9f).instrument(NoteBlockInstrument.CHIME));
+	}
+
+	@Override
+	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
+		return 14;
+	}
+
+	@Override
+	public int getExpDrop(BlockState state, LevelReader level, RandomSource randomSource, BlockPos pos, int fortuneLevel, int silkTouchLevel) {
+		return Mth.randomBetweenInclusive(randomSource, 0, 2);
+	}
+}
