81c1f75ea348c33b34442d334684e4d04719f538
xlebore3o4ka <xlebore3o4ka@gmail.com>
2026-03-02 21:50:22 +0700

    delete gex fr

--- src/main/java/net/mcreator/psoe/init/PsoeModBlocks.java
+++ src/main/java/net/mcreator/psoe/init/PsoeModBlocks.java
@@ -10,7 +10,6 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraft.world.level.block.Block;
 
 import net.mcreator.psoe.block.TestBlock;
-import net.mcreator.psoe.block.GexBlockBlock;
 import net.mcreator.psoe.block.FurnaceInterfaceBlock;
 import net.mcreator.psoe.block.CinnabarBlock;
 import net.mcreator.psoe.PsoeMod;
@@ -20,12 +19,10 @@ public class PsoeModBlocks {
 	public static final RegistryObject<Block> TEST;
 	public static final RegistryObject<Block> CINNABAR;
 	public static final RegistryObject<Block> FURNACE_INTERFACE;
-	public static final RegistryObject<Block> GEX_BLOCK;
 	static {
 		TEST = REGISTRY.register("test", TestBlock::new);
 		CINNABAR = REGISTRY.register("cinnabar", CinnabarBlock::new);
 		FURNACE_INTERFACE = REGISTRY.register("furnace_interface", FurnaceInterfaceBlock::new);
-		GEX_BLOCK = REGISTRY.register("gex_block", GexBlockBlock::new);
 	}
 	// Start of user code block custom blocks
 	// End of user code block custom blocks
